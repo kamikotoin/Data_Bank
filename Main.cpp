82b32aaa3339d8edb278ab6e5bbc20bccaf333be
1b31d107 <1b31d107@gmail.com>
2025-11-26 13:18:23 +0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,20 @@
-using namespace std;
 #include <iostream>
-#include "Include/Node.h";
+#include "Node.h"
+using namespace std;
+
 
 int main() {
-    account_Node* root=new account_Node();
-    root->SetAccountId(50);
-    root->SetName("RootAccount");
-    root->SetBalance(1000.0);
-    
-    addAccount(root, new account_Node());
-    
-    return 0;
+	TransactionHistory a(
+		new TransactionNode("Deposit", 2000, "10/30/2025")
+	);
+
+	a.addTransaction("Deposit", 20, "11/01/2025");
+	a.addTransaction("Withdrawal", 40, "11/05/2025");
+	a.addTransaction("Deposit", 50, "11/12/2025");
+	a.addTransaction("Withdrawal", 100, "11/17/2025");
+	a.addTransaction("Deposit", 999, "11/26/2025");
+
+	cout << "Number of Transactions: " << a.countTransactions() << endl;
+
+	a.printHistory();
 }
